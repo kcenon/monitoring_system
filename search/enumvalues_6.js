e2d147ceb98a1f385e73a8154ef686e2e2e95765
kcenon <kcenon@users.noreply.github.com>
2025-09-11 02:10:42 +0000

    Update documentation via GitHub Actions 542a84b2646d4386807f26491137b3a16e0a26ec

--- search/enumvalues_6.js
+++ search/enumvalues_6.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['metric_5fnot_5ffound_0',['metric_not_found',['../de/d3f/namespacemonitoring__system.html#a22dca6fa0d74ff9143a6386fbd724ecca56d6e1482db93bec9f173a8a021b9735',1,'monitoring_system']]],
-  ['metric_5foverflow_1',['metric_overflow',['../de/d3f/namespacemonitoring__system.html#a22dca6fa0d74ff9143a6386fbd724eccad611c462410db25662c51847fedd0926',1,'monitoring_system']]],
-  ['monitoring_5fdisabled_2',['monitoring_disabled',['../de/d3f/namespacemonitoring__system.html#a22dca6fa0d74ff9143a6386fbd724eccae844dc5bd67e42c521cf417f5db01f11',1,'monitoring_system']]]
+  ['memory_5fallocation_5ffailed_0',['memory_allocation_failed',['../de/d3f/namespacemonitoring__system.html#a22dca6fa0d74ff9143a6386fbd724eccabac5a7f5a8055a7ebd356e87be3179bf',1,'monitoring_system']]],
+  ['metric_5fnot_5ffound_1',['metric_not_found',['../de/d3f/namespacemonitoring__system.html#a22dca6fa0d74ff9143a6386fbd724ecca56d6e1482db93bec9f173a8a021b9735',1,'monitoring_system']]],
+  ['metric_5foverflow_2',['metric_overflow',['../de/d3f/namespacemonitoring__system.html#a22dca6fa0d74ff9143a6386fbd724eccad611c462410db25662c51847fedd0926',1,'monitoring_system']]],
+  ['monitoring_5fdisabled_3',['monitoring_disabled',['../de/d3f/namespacemonitoring__system.html#a22dca6fa0d74ff9143a6386fbd724eccae844dc5bd67e42c521cf417f5db01f11',1,'monitoring_system']]]
 ];
--- search/enumvalues_9.js
+++ search/enumvalues_9.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['permission_5fdenied_0',['permission_denied',['../de/d3f/namespacemonitoring__system.html#a22dca6fa0d74ff9143a6386fbd724eccae8ee838822072f02738dbd7d97ea03fc',1,'monitoring_system']]]
+  ['permission_5fdenied_0',['permission_denied',['../de/d3f/namespacemonitoring__system.html#a22dca6fa0d74ff9143a6386fbd724eccae8ee838822072f02738dbd7d97ea03fc',1,'monitoring_system']]],
+  ['processing_5ffailed_1',['processing_failed',['../de/d3f/namespacemonitoring__system.html#a22dca6fa0d74ff9143a6386fbd724eccad1d24eb94ee39e41e17b65cc63118672',1,'monitoring_system']]]
 ];
