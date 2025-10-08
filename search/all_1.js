e7d44e96ce3c0d3b214c17a226917c527c1685d8
kcenon <kcenon@users.noreply.github.com>
2025-10-08 04:51:21 +0000

    Update documentation via GitHub Actions b29e23b5ced10f9357c19a06a8d76a73ba892030

--- search/all_1.js
+++ search/all_1.js
@@ -3,5 +3,5 @@ var searchData=
   ['basic_5fmonitoring_5fexample_2ecpp_0',['basic_monitoring_example.cpp',['../d0/d97/basic__monitoring__example_8cpp.html',1,'']]],
   ['bidirectional_5fdi_5fexample_2ecpp_1',['bidirectional_di_example.cpp',['../d7/d24/bidirectional__di__example_8cpp.html',1,'']]],
   ['bufferingstrategiestest_2',['BufferingStrategiesTest',['../d9/dee/classBufferingStrategiesTest.html',1,'']]],
-  ['bus_3',['bus',['../d7/d12/classEventBusTest.html#aef1ae57c23ecf661d205c920005bc5b9',1,'EventBusTest']]]
+  ['bus_3',['bus',['../d7/d12/classEventBusTest.html#aef1ae57c23ecf661d205c920005bc5b9',1,'EventBusTest::bus'],['../d9/d57/classMonitoringThreadSafetyTest.html#aacec7d68f0d3099ecb3f0b9120791271',1,'MonitoringThreadSafetyTest::bus']]]
 ];
--- search/variables_0.js
+++ search/variables_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['bus_0',['bus',['../d7/d12/classEventBusTest.html#aef1ae57c23ecf661d205c920005bc5b9',1,'EventBusTest']]]
+  ['bus_0',['bus',['../d7/d12/classEventBusTest.html#aef1ae57c23ecf661d205c920005bc5b9',1,'EventBusTest::bus'],['../d9/d57/classMonitoringThreadSafetyTest.html#aacec7d68f0d3099ecb3f0b9120791271',1,'MonitoringThreadSafetyTest::bus']]]
 ];
