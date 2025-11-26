516421a1cb4f620b1101dc8419df0abcc1d57439
kcenon <kcenon@users.noreply.github.com>
2025-11-26 02:31:42 +0000

    Update documentation via GitHub Actions 4e18e7d46f1a8f6ff2a169d799cefbd816ed788d

--- search/all_e.js
+++ search/all_e.js
@@ -6,9 +6,11 @@ var searchData=
   ['performance_5fmonitor_5fimpl_3',['performance_monitor_impl',['../db/d7f/classperformance__monitor__impl.html',1,'performance_monitor_impl'],['../db/d7f/classperformance__monitor__impl.html#a6315e0bdd0d18ea9dbb307769479aeff',1,'performance_monitor_impl::performance_monitor_impl()']]],
   ['performance_5fmonitor_5fold_4',['performance_monitor_old',['../d2/d91/classperformance__monitor__old.html',1,'performance_monitor_old'],['../d2/d91/classperformance__monitor__old.html#ae54a9fde689e763919973425509b5026',1,'performance_monitor_old::performance_monitor_old()']]],
   ['performancemonitoringtest_5',['PerformanceMonitoringTest',['../d7/dbe/classPerformanceMonitoringTest.html',1,'']]],
-  ['plugin_5fcollector_5fexample_2ecpp_6',['plugin_collector_example.cpp',['../dc/d7a/plugin__collector__example_8cpp.html',1,'']]],
-  ['prefix_5f_7',['prefix_',['../d3/dfa/classexample__logger.html#ac81ee8f3c5faa9939f1d2b71ab04f58e',1,'example_logger']]],
-  ['process_5fbusiness_5flogic_8',['process_business_logic',['../df/d65/classWebService.html#a596c1a2053455df6df50c850e3828532',1,'WebService']]],
-  ['process_5fmetric_9',['process_metric',['../de/dfb/result__pattern__example_8cpp.html#a92da1737d943d0e99e570c8ca59abc75',1,'result_pattern_example.cpp']]],
-  ['profiler_10',['profiler',['../d7/dbe/classPerformanceMonitoringTest.html#a47d07edd9fd1df796473642030bb2b24',1,'PerformanceMonitoringTest']]]
+  ['performancemonitorthreadsafetytest_6',['PerformanceMonitorThreadSafetyTest',['../d8/d32/classPerformanceMonitorThreadSafetyTest.html',1,'']]],
+  ['performanceprofilerthreadsafetytest_7',['PerformanceProfilerThreadSafetyTest',['../df/d0f/classPerformanceProfilerThreadSafetyTest.html',1,'']]],
+  ['plugin_5fcollector_5fexample_2ecpp_8',['plugin_collector_example.cpp',['../dc/d7a/plugin__collector__example_8cpp.html',1,'']]],
+  ['prefix_5f_9',['prefix_',['../d3/dfa/classexample__logger.html#ac81ee8f3c5faa9939f1d2b71ab04f58e',1,'example_logger']]],
+  ['process_5fbusiness_5flogic_10',['process_business_logic',['../df/d65/classWebService.html#a596c1a2053455df6df50c850e3828532',1,'WebService']]],
+  ['process_5fmetric_11',['process_metric',['../de/dfb/result__pattern__example_8cpp.html#a92da1737d943d0e99e570c8ca59abc75',1,'result_pattern_example.cpp']]],
+  ['profiler_12',['profiler',['../d7/dbe/classPerformanceMonitoringTest.html#a47d07edd9fd1df796473642030bb2b24',1,'PerformanceMonitoringTest::profiler'],['../df/d0f/classPerformanceProfilerThreadSafetyTest.html#ac48a8245dd958956f927241719ec3b93',1,'PerformanceProfilerThreadSafetyTest::profiler']]]
 ];
--- search/classes_b.js
+++ search/classes_b.js
@@ -3,5 +3,7 @@ var searchData=
   ['performance_5fmonitor_5ffacade_0',['performance_monitor_facade',['../d9/d17/classperformance__monitor__facade.html',1,'']]],
   ['performance_5fmonitor_5fimpl_1',['performance_monitor_impl',['../db/d7f/classperformance__monitor__impl.html',1,'']]],
   ['performance_5fmonitor_5fold_2',['performance_monitor_old',['../d2/d91/classperformance__monitor__old.html',1,'']]],
-  ['performancemonitoringtest_3',['PerformanceMonitoringTest',['../d7/dbe/classPerformanceMonitoringTest.html',1,'']]]
+  ['performancemonitoringtest_3',['PerformanceMonitoringTest',['../d7/dbe/classPerformanceMonitoringTest.html',1,'']]],
+  ['performancemonitorthreadsafetytest_4',['PerformanceMonitorThreadSafetyTest',['../d8/d32/classPerformanceMonitorThreadSafetyTest.html',1,'']]],
+  ['performanceprofilerthreadsafetytest_5',['PerformanceProfilerThreadSafetyTest',['../df/d0f/classPerformanceProfilerThreadSafetyTest.html',1,'']]]
 ];
