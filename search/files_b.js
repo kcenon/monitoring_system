b6aea8f37e9926435db16edb664e02046b406182
kcenon <kcenon@users.noreply.github.com>
2025-12-10 00:22:29 +0000

    Update documentation via GitHub Actions 9d66e41bfd0e91f9421d5b533a54ec4be40b63ae

--- search/files_b.js
+++ search/files_b.js
@@ -43,6 +43,7 @@ var searchData=
   ['test_5fthread_5fcontext_5fsimple_2ecpp_40',['test_thread_context_simple.cpp',['../de/db1/test__thread__context__simple_8cpp.html',1,'']]],
   ['test_5ftimer_5fmetrics_2ecpp_41',['test_timer_metrics.cpp',['../d4/df9/test__timer__metrics_8cpp.html',1,'']]],
   ['test_5ftrace_5fexporters_2ecpp_42',['test_trace_exporters.cpp',['../dc/dbd/test__trace__exporters_8cpp.html',1,'']]],
-  ['test_5fvm_5fcollector_2ecpp_43',['test_vm_collector.cpp',['../dd/d57/test__vm__collector_8cpp.html',1,'']]],
-  ['thread_5fsafety_5ftests_2ecpp_44',['thread_safety_tests.cpp',['../df/d66/thread__safety__tests_8cpp.html',1,'']]]
+  ['test_5fuptime_5fcollector_2ecpp_43',['test_uptime_collector.cpp',['../df/dd9/test__uptime__collector_8cpp.html',1,'']]],
+  ['test_5fvm_5fcollector_2ecpp_44',['test_vm_collector.cpp',['../dd/d57/test__vm__collector_8cpp.html',1,'']]],
+  ['thread_5fsafety_5ftests_2ecpp_45',['thread_safety_tests.cpp',['../df/d66/thread__safety__tests_8cpp.html',1,'']]]
 ];
