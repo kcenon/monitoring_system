01007b9a638b284a5b1aee0b9cfbdf50e59d02c6
kcenon <kcenon@users.noreply.github.com>
2026-01-08 14:20:10 +0000

    Update documentation via GitHub Actions 1258690304e62bca4baaaeda342ffc99e7beb7e3

--- search/classes_10.js
+++ search/classes_10.js
@@ -5,5 +5,8 @@ var searchData=
   ['resourcemanagementtest_2',['ResourceManagementTest',['../d7/d4c/classResourceManagementTest.html',1,'']]],
   ['resulttypestest_3',['ResultTypesTest',['../d3/d70/classResultTypesTest.html',1,'']]],
   ['retry_5fconfig_4',['retry_config',['../d4/d7d/structkcenon_1_1monitoring_1_1retry__config.html',1,'kcenon::monitoring']]],
-  ['retry_5fpolicy_5',['retry_policy',['../d3/d0a/classkcenon_1_1monitoring_1_1retry__policy.html',1,'kcenon::monitoring']]]
+  ['retry_5fexecutor_5',['retry_executor',['../d9/d1a/classkcenon_1_1monitoring_1_1retry__executor.html',1,'kcenon::monitoring']]],
+  ['retry_5fexecutor_5fregistry_6',['retry_executor_registry',['../da/d65/classkcenon_1_1monitoring_1_1retry__executor__registry.html',1,'kcenon::monitoring']]],
+  ['retry_5fmetrics_7',['retry_metrics',['../dc/de7/structkcenon_1_1monitoring_1_1retry__metrics.html',1,'kcenon::monitoring']]],
+  ['retry_5fpolicy_8',['retry_policy',['../d3/d0a/classkcenon_1_1monitoring_1_1retry__policy.html',1,'kcenon::monitoring']]]
 ];
--- search/classes_5.js
+++ search/classes_5.js
@@ -1,12 +1,15 @@
 var searchData=
 [
-  ['fault_5ftolerance_5fmanager_0',['fault_tolerance_manager',['../d2/d74/classkcenon_1_1monitoring_1_1fault__tolerance__manager.html',1,'kcenon::monitoring']]],
-  ['faulttolerancetest_1',['FaultToleranceTest',['../d4/d91/classFaultToleranceTest.html',1,'']]],
-  ['fd_5fcollector_2',['fd_collector',['../db/d5d/classkcenon_1_1monitoring_1_1fd__collector.html',1,'kcenon::monitoring']]],
-  ['fd_5finfo_3',['fd_info',['../d4/de8/structkcenon_1_1monitoring_1_1platform_1_1fd__info.html',1,'kcenon::monitoring::platform']]],
-  ['fd_5finfo_5fcollector_4',['fd_info_collector',['../d8/d4c/classkcenon_1_1monitoring_1_1fd__info__collector.html',1,'kcenon::monitoring']]],
-  ['fd_5fmetrics_5',['fd_metrics',['../d7/d07/structkcenon_1_1monitoring_1_1fd__metrics.html',1,'kcenon::monitoring']]],
-  ['file_5fstorage_5fbackend_6',['file_storage_backend',['../df/d62/classkcenon_1_1monitoring_1_1file__storage__backend.html',1,'kcenon::monitoring']]],
-  ['filesystem_5finode_5finfo_7',['filesystem_inode_info',['../d2/db1/structkcenon_1_1monitoring_1_1filesystem__inode__info.html',1,'kcenon::monitoring']]],
-  ['functional_5fhealth_5fcheck_8',['functional_health_check',['../d7/dfb/classkcenon_1_1monitoring_1_1functional__health__check.html',1,'kcenon::monitoring']]]
+  ['fault_5ftolerance_5fconfig_0',['fault_tolerance_config',['../d8/de8/structkcenon_1_1monitoring_1_1fault__tolerance__config.html',1,'kcenon::monitoring']]],
+  ['fault_5ftolerance_5fmanager_1',['fault_tolerance_manager',['../d2/d74/classkcenon_1_1monitoring_1_1fault__tolerance__manager.html',1,'kcenon::monitoring']]],
+  ['fault_5ftolerance_5fmetrics_2',['fault_tolerance_metrics',['../df/d26/structkcenon_1_1monitoring_1_1fault__tolerance__metrics.html',1,'kcenon::monitoring']]],
+  ['fault_5ftolerance_5fregistry_3',['fault_tolerance_registry',['../dc/d94/classkcenon_1_1monitoring_1_1fault__tolerance__registry.html',1,'kcenon::monitoring']]],
+  ['faulttolerancetest_4',['FaultToleranceTest',['../d4/d91/classFaultToleranceTest.html',1,'']]],
+  ['fd_5fcollector_5',['fd_collector',['../db/d5d/classkcenon_1_1monitoring_1_1fd__collector.html',1,'kcenon::monitoring']]],
+  ['fd_5finfo_6',['fd_info',['../d4/de8/structkcenon_1_1monitoring_1_1platform_1_1fd__info.html',1,'kcenon::monitoring::platform']]],
+  ['fd_5finfo_5fcollector_7',['fd_info_collector',['../d8/d4c/classkcenon_1_1monitoring_1_1fd__info__collector.html',1,'kcenon::monitoring']]],
+  ['fd_5fmetrics_8',['fd_metrics',['../d7/d07/structkcenon_1_1monitoring_1_1fd__metrics.html',1,'kcenon::monitoring']]],
+  ['file_5fstorage_5fbackend_9',['file_storage_backend',['../df/d62/classkcenon_1_1monitoring_1_1file__storage__backend.html',1,'kcenon::monitoring']]],
+  ['filesystem_5finode_5finfo_10',['filesystem_inode_info',['../d2/db1/structkcenon_1_1monitoring_1_1filesystem__inode__info.html',1,'kcenon::monitoring']]],
+  ['functional_5fhealth_5fcheck_11',['functional_health_check',['../d7/dfb/classkcenon_1_1monitoring_1_1functional__health__check.html',1,'kcenon::monitoring']]]
 ];
