cb4652314161056b32490d63910a69211c6776a6
kcenon <kcenon@users.noreply.github.com>
2026-01-09 00:42:17 +0000

    Update documentation via GitHub Actions 4baf55aee876c1456a4b51b37fa4dabc81a16734

--- search/classes_7.js
+++ search/classes_7.js
@@ -1,15 +1,19 @@
 var searchData=
 [
   ['handler_5ferror_5finfo_0',['handler_error_info',['../d0/ddf/structkcenon_1_1monitoring_1_1handler__error__info.html',1,'kcenon::monitoring']]],
-  ['health_5fcheck_5fevent_1',['health_check_event',['../d2/d8d/classkcenon_1_1monitoring_1_1health__check__event.html',1,'kcenon::monitoring']]],
-  ['health_5fcheck_5fresult_2',['health_check_result',['../d3/d6e/structkcenon_1_1monitoring_1_1health__check__event_1_1health__check__result.html',1,'kcenon::monitoring::health_check_event::health_check_result'],['../dd/d56/structkcenon_1_1monitoring_1_1health__check__result.html',1,'kcenon::monitoring::health_check_result']]],
-  ['health_5fmonitor_3',['health_monitor',['../d5/d42/classkcenon_1_1monitoring_1_1health__monitor.html',1,'kcenon::monitoring']]],
-  ['health_5fmonitor_5fconfig_4',['health_monitor_config',['../de/dba/structkcenon_1_1monitoring_1_1health__monitor__config.html',1,'kcenon::monitoring']]],
-  ['healthmonitoringtest_5',['HealthMonitoringTest',['../da/d18/classHealthMonitoringTest.html',1,'']]],
-  ['histogram_5fbucket_6',['histogram_bucket',['../dd/d90/structkcenon_1_1monitoring_1_1histogram__bucket.html',1,'kcenon::monitoring']]],
-  ['histogram_5fdata_7',['histogram_data',['../d4/da9/structkcenon_1_1monitoring_1_1histogram__data.html',1,'kcenon::monitoring']]],
-  ['histogrammetricstest_8',['HistogramMetricsTest',['../d0/d7d/classHistogramMetricsTest.html',1,'']]],
-  ['http_5frequest_9',['http_request',['../d4/d99/structkcenon_1_1monitoring_1_1http__request.html',1,'kcenon::monitoring']]],
-  ['http_5fresponse_10',['http_response',['../da/d05/structkcenon_1_1monitoring_1_1http__response.html',1,'kcenon::monitoring']]],
-  ['http_5ftransport_11',['http_transport',['../de/d16/classkcenon_1_1monitoring_1_1http__transport.html',1,'kcenon::monitoring']]]
+  ['health_5fcheck_1',['health_check',['../da/d96/classkcenon_1_1monitoring_1_1health__check.html',1,'kcenon::monitoring']]],
+  ['health_5fcheck_5fbuilder_2',['health_check_builder',['../d2/d7c/classkcenon_1_1monitoring_1_1health__check__builder.html',1,'kcenon::monitoring']]],
+  ['health_5fcheck_5fevent_3',['health_check_event',['../d2/d8d/classkcenon_1_1monitoring_1_1health__check__event.html',1,'kcenon::monitoring']]],
+  ['health_5fcheck_5fresult_4',['health_check_result',['../d3/d6e/structkcenon_1_1monitoring_1_1health__check__event_1_1health__check__result.html',1,'kcenon::monitoring::health_check_event::health_check_result'],['../dd/d56/structkcenon_1_1monitoring_1_1health__check__result.html',1,'kcenon::monitoring::health_check_result']]],
+  ['health_5fdependency_5fgraph_5',['health_dependency_graph',['../d8/d8e/classkcenon_1_1monitoring_1_1health__dependency__graph.html',1,'kcenon::monitoring']]],
+  ['health_5fmonitor_6',['health_monitor',['../d5/d42/classkcenon_1_1monitoring_1_1health__monitor.html',1,'kcenon::monitoring']]],
+  ['health_5fmonitor_5fconfig_7',['health_monitor_config',['../de/dba/structkcenon_1_1monitoring_1_1health__monitor__config.html',1,'kcenon::monitoring']]],
+  ['health_5fmonitor_5fstats_8',['health_monitor_stats',['../de/d31/structkcenon_1_1monitoring_1_1health__monitor__stats.html',1,'kcenon::monitoring']]],
+  ['healthmonitoringtest_9',['HealthMonitoringTest',['../da/d18/classHealthMonitoringTest.html',1,'']]],
+  ['histogram_5fbucket_10',['histogram_bucket',['../dd/d90/structkcenon_1_1monitoring_1_1histogram__bucket.html',1,'kcenon::monitoring']]],
+  ['histogram_5fdata_11',['histogram_data',['../d4/da9/structkcenon_1_1monitoring_1_1histogram__data.html',1,'kcenon::monitoring']]],
+  ['histogrammetricstest_12',['HistogramMetricsTest',['../d0/d7d/classHistogramMetricsTest.html',1,'']]],
+  ['http_5frequest_13',['http_request',['../d4/d99/structkcenon_1_1monitoring_1_1http__request.html',1,'kcenon::monitoring']]],
+  ['http_5fresponse_14',['http_response',['../da/d05/structkcenon_1_1monitoring_1_1http__response.html',1,'kcenon::monitoring']]],
+  ['http_5ftransport_15',['http_transport',['../de/d16/classkcenon_1_1monitoring_1_1http__transport.html',1,'kcenon::monitoring']]]
 ];
--- search/functions_16.js
+++ search/functions_16.js
@@ -1,6 +1,12 @@
 var searchData=
 [
   ['webservice_0',['WebService',['../df/d65/classWebService.html#a6f92342771e33bdec3ffb4fecd581801',1,'WebService']]],
-  ['write_5ffixed64_1',['write_fixed64',['../d1/d45/classkcenon_1_1monitoring_1_1otlp__span__converter.html#aaf1655480c90296dea76b44952d6f775',1,'kcenon::monitoring::otlp_span_converter']]],
-  ['write_5fvarint_2',['write_varint',['../d1/d45/classkcenon_1_1monitoring_1_1otlp__span__converter.html#ade80dc81ce7c0f53e66191ef2ea78ef3',1,'kcenon::monitoring::otlp_span_converter']]]
+  ['with_5fcheck_1',['with_check',['../d2/d7c/classkcenon_1_1monitoring_1_1health__check__builder.html#a601b30b9c3dcd3ef36f0b43f2ec780d4',1,'kcenon::monitoring::health_check_builder']]],
+  ['with_5fname_2',['with_name',['../d2/d7c/classkcenon_1_1monitoring_1_1health__check__builder.html#acf179134819876fb28cc1da173e1c339',1,'kcenon::monitoring::health_check_builder']]],
+  ['with_5ftimeout_3',['with_timeout',['../d2/d7c/classkcenon_1_1monitoring_1_1health__check__builder.html#af5c373c54249867a2d55e2c23ee6247b',1,'kcenon::monitoring::health_check_builder']]],
+  ['with_5ftype_4',['with_type',['../d2/d7c/classkcenon_1_1monitoring_1_1health__check__builder.html#a95e3042b5e63495808700d4a9652ba9f',1,'kcenon::monitoring::health_check_builder']]],
+  ['would_5fcreate_5fcycle_5',['would_create_cycle',['../d8/d8e/classkcenon_1_1monitoring_1_1health__dependency__graph.html#a2cd2b3cd07fe9218572ebfb276fcc23a',1,'kcenon::monitoring::health_dependency_graph']]],
+  ['would_5fcreate_5fcycle_5finternal_6',['would_create_cycle_internal',['../d8/d8e/classkcenon_1_1monitoring_1_1health__dependency__graph.html#a286e02ef975b5f338d1c5876d2ba0e6d',1,'kcenon::monitoring::health_dependency_graph']]],
+  ['write_5ffixed64_7',['write_fixed64',['../d1/d45/classkcenon_1_1monitoring_1_1otlp__span__converter.html#aaf1655480c90296dea76b44952d6f775',1,'kcenon::monitoring::otlp_span_converter']]],
+  ['write_5fvarint_8',['write_varint',['../d1/d45/classkcenon_1_1monitoring_1_1otlp__span__converter.html#ade80dc81ce7c0f53e66191ef2ea78ef3',1,'kcenon::monitoring::otlp_span_converter']]]
 ];
