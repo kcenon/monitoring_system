c09650c787a7d0bce4c09a843bfd45afd596f38a
kcenon <kcenon@users.noreply.github.com>
2026-01-21 16:22:28 +0000

    Update documentation via GitHub Actions 51394a54f680881a4958379ca25f1eacf882146e

--- search/files_d.js
+++ search/files_d.js
@@ -3,8 +3,9 @@ var searchData=
   ['performance_5fmonitor_2eh_0',['performance_monitor.h',['../d7/db1/performance__monitor_8h.html',1,'']]],
   ['performance_5fmonitor_5fadapter_2eh_1',['performance_monitor_adapter.h',['../d0/daf/performance__monitor__adapter_8h.html',1,'']]],
   ['performance_5ftypes_2eh_2',['performance_types.h',['../d3/d47/performance__types_8h.html',1,'']]],
-  ['plugin_5fcollector_5fexample_2ecpp_3',['plugin_collector_example.cpp',['../dc/d7a/plugin__collector__example_8cpp.html',1,'']]],
-  ['plugin_5fmetric_5fcollector_2eh_4',['plugin_metric_collector.h',['../d6/da5/plugin__metric__collector_8h.html',1,'']]],
-  ['power_5fcollector_2eh_5',['power_collector.h',['../d0/d4c/power__collector_8h.html',1,'']]],
-  ['process_5fmetrics_5fcollector_2eh_6',['process_metrics_collector.h',['../de/ded/process__metrics__collector_8h.html',1,'']]]
+  ['platform_5fmetrics_5fcollector_2eh_3',['platform_metrics_collector.h',['../d1/dc2/platform__metrics__collector_8h.html',1,'']]],
+  ['plugin_5fcollector_5fexample_2ecpp_4',['plugin_collector_example.cpp',['../dc/d7a/plugin__collector__example_8cpp.html',1,'']]],
+  ['plugin_5fmetric_5fcollector_2eh_5',['plugin_metric_collector.h',['../d6/da5/plugin__metric__collector_8h.html',1,'']]],
+  ['power_5fcollector_2eh_6',['power_collector.h',['../d0/d4c/power__collector_8h.html',1,'']]],
+  ['process_5fmetrics_5fcollector_2eh_7',['process_metrics_collector.h',['../de/ded/process__metrics__collector_8h.html',1,'']]]
 ];
